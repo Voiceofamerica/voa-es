93e1211eadc4ecc13be41a4546f0e356c2483f2a
Kevin Groat <apps@kevingroat.io>
2018-07-24 14:01:28 -0400

    Added PsiphonLoading screen

--- src/labels.tsx
+++ src/labels.tsx
@@ -120,6 +120,11 @@ export const programsScreenLabels = {
   empty: 'No hay contenidos',
 }
 
+export const psiphonLoadingLabels = {
+  bold: 'Please be patient.',
+  text: 'This may take a few minutes while we gather the best information for you.',
+}
+
 export const pullToRefreshLabels = {
   pull: 'Deslizar hacia abajo para refrescar',
   release: 'Deslizar hacia abajo para refrescar',
